792dd4014ef576f5cf8033fc712da5f59f2dbb40
sorianoribalda <61832805+sorianoribalda@users.noreply.github.com>
2020-03-05 13:48:26 +0100

    Mi primer commit

---
+++ bio/scripts/script.js
@@ -0,0 +1,5 @@
+function fondoAccesible(){
+    document.body.style.backgroundColor='black';
+    document.body.style.color='white'
+}
+document.querySelector('h1').onclick= function() { alert('¡Jobar! ¡Deja de pincharme!')}
